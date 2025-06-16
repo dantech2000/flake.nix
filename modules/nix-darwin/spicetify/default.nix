315678a23da11a5779ef25a9b5882dceea2f17fc
Daniel Rodriguez <drodriguez@codecademy.com>
2025-06-16 15:57:45 -0700

    chore: liniting fix

--- modules/nix-darwin/spicetify/default.nix
+++ modules/nix-darwin/spicetify/default.nix
@@ -1,47 +1,46 @@
-{ 
-  inputs, 
-  pkgs, 
-  lib, 
-  ... 
-}: 
+{ inputs
+, pkgs
+, lib
+, ...
+}:
 let
   spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
 in
 {
-  imports = [ inputs.spicetify-nix.darwinModules.spicetify];
+  imports = [ inputs.spicetify-nix.darwinModules.spicetify ];
 
-   programs.spicetify = {
-     enable = true;
-     theme = spicePkgs.themes.text;
-     colorScheme = "TokyoNight";
-     enabledExtensions = with spicePkgs.extensions; [
-        adblock
-        autoVolume
-        copyToClipboard
-        fullAlbumDate
-        fullAppDisplay
-        goToSong
-        groupSession
-        hidePodcasts
-        history
-        keyboardShortcut
-        lastfm
-        listPlaylistsWithSong
-        loopyLoop
-        phraseToPlaylist
-        playNext
-        playlistIcons
-        playlistIntersection
-        popupLyrics
-        savePlaylists
-        shuffle # shuffle+
-        volumePercentage
-     ];
-     enabledCustomApps = with spicePkgs.apps; [
-        newReleases
-        lyricsPlus
-        ncsVisualizer
-        marketplace
-      ];
-   };
+  programs.spicetify = {
+    enable = true;
+    theme = spicePkgs.themes.text;
+    colorScheme = "TokyoNight";
+    enabledExtensions = with spicePkgs.extensions; [
+      adblock
+      autoVolume
+      copyToClipboard
+      fullAlbumDate
+      fullAppDisplay
+      goToSong
+      groupSession
+      hidePodcasts
+      history
+      keyboardShortcut
+      lastfm
+      listPlaylistsWithSong
+      loopyLoop
+      phraseToPlaylist
+      playNext
+      playlistIcons
+      playlistIntersection
+      popupLyrics
+      savePlaylists
+      shuffle # shuffle+
+      volumePercentage
+    ];
+    enabledCustomApps = with spicePkgs.apps; [
+      newReleases
+      lyricsPlus
+      ncsVisualizer
+      marketplace
+    ];
+  };
 }
