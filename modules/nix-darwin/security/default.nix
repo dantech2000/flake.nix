7f57f04c0524ec4a55f06eaf390d0f5c2bb689b7
Daniel Rodriguez <drodriguez@codecademy.com>
2025-09-21 02:11:29 -0700

    wip - Remove firewall config for darwin

--- modules/nix-darwin/security/default.nix
+++ modules/nix-darwin/security/default.nix
@@ -1,4 +1,7 @@
-_: {
+{ inputs, ... }: {
   # TouchID authentication for sudo
   security.pam.services.sudo_local.touchIdAuth = true;
+
+  # Do not try to reconfigure the macOS firewall; managed Macs block CLI changes
+  disabledModules = ["${inputs.nix-darwin.outPath}/modules/networking/applicationFirewall.nix"];
 }
